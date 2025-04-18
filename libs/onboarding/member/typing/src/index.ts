881114032018d973a5439d6de2694e7318b41b1b
ThaisRobba <thais.robba@gmail.com>
2025-04-18 12:42:23 -0300

    chore: assemble min reproducible error state

---
+++ libs/onboarding/member/typing/src/index.ts
@@ -0,0 +1,14 @@
+export * from './lib/enums/account-connection-status';
+export * from './lib/enums/account-types';
+export * from './lib/constants/onboarding-routes-config';
+export * from './lib/types/account';
+export * from './lib/types/analytics-events';
+export * from './lib/types/animation';
+export * from './lib/types/attracking-analytics-events';
+export * from './lib/types/configurable-onboarding';
+export * from './lib/types/free-trial-subscription-plans';
+export * from './lib/types/shared';
+export * from './lib/types/configurable-onboarding';
+export * from './lib/constants/free-trial-features-list';
+export * from './lib/constants/onboarding-versions-config';
+export { ConfigurableOnboardingKeys } from './lib/enums/configurable-onboarding-keys';
